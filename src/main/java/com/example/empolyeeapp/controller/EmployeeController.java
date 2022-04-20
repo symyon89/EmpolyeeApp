d0c9542ec9b5f2b5db4142ea6b8776bc40f4fb2d
George <symyon89@yahoo.com>
2022-04-20 23:27:25 +0300

    origin

--- src/main/java/com/example/empolyeeapp/controller/EmployeeController.java
+++ src/main/java/com/example/empolyeeapp/controller/EmployeeController.java
@@ -14,6 +14,7 @@ import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @RequiredArgsConstructor
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("employee")
 public class EmployeeController {
 
